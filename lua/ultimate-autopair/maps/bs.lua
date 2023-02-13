dbb229181f1f69522418f686f9b2767a2935a33a
altermo <>
2023-02-13 20:08:51 +0100

    M lua/ultimate-autopair/maps/bs.lua

--- lua/ultimate-autopair/maps/bs.lua
+++ lua/ultimate-autopair/maps/bs.lua
@@ -6,49 +6,49 @@ local mem=require'ultimate-autopair.memory'
 local utils=require'ultimate-autopair.utils.utils'
 local info_line=require'ultimate-autopair.utils.info_line'
 local open_pair=require'ultimate-autopair.utils.open_pair'
-function M.extensions.delete_mid_pair(prev_char,next_char,line,col)
-  if mem.ispair(prev_char,next_char) then
-    if mem.mem[prev_char].type==3 then
-      if not open_pair.open_pair_ambigous(prev_char,line) then
+function M.extensions.delete_mid_pair(o)
+  if mem.ispair(o.prev_char,o.next_char) then
+    if mem.mem[o.prev_char].type==3 then
+      if not open_pair.open_pair_ambigous(o.prev_char,o.line) then
         return utils.delete(1,1)
       end
-    elseif not open_pair.open_pair_before(prev_char,next_char,line,col) then
+    elseif not open_pair.open_pair_before(o.prev_char,o.next_char,o.line,o.col) then
       return utils.delete(1,1)
     end
   end
 end
-function M.extensions.delete_prev_pair(prev_char,_,line,col)
-  local prev_pair=mem.mem[prev_char]
+function M.extensions.delete_prev_pair(o)
+  local prev_pair=mem.mem[o.prev_char]
   if prev_pair and prev_pair.type~=1 then
-    local prev_2_char=line:sub(col-2,col-2)
-    if mem.ispair(prev_2_char,prev_char) then
+    local prev_2_char=o.line:sub(o.col-2,o.col-2)
+    if mem.ispair(prev_2_char,o.prev_char) then
       if prev_pair.type==3 then
-        if not open_pair.open_pair_ambigous(prev_char,line) then
+        if not open_pair.open_pair_ambigous(o.prev_char,o.line) then
           return utils.delete(2)
         end
-      elseif not open_pair.open_paire_after(prev_2_char,prev_char,line,col) then
+      elseif not open_pair.open_paire_after(prev_2_char,o.prev_char,o.line,o.col) then
         return utils.delete(2)
       end
     end
   end
 end
-function M.extensions.delete_overjump_pair(prev_char,_,line,col)
-  local prev_pair=mem.mem[prev_char]
+function M.extensions.delete_overjump_pair(o)
+  local prev_pair=mem.mem[o.prev_char]
   if M.conf.overjump and prev_pair and prev_pair.type==1 then
-    if not open_pair.open_pair_before(prev_char,prev_pair.paire,line,col) then
-      local matching_pair_pos=info_line.findepaire(line,col,prev_char,prev_pair.paire)
+    if not open_pair.open_pair_before(o.prev_char,prev_pair.paire,o.line,o.col) then
+      local matching_pair_pos=info_line.findepaire(o.line,o.col,o.prev_char,prev_pair.paire)
       if matching_pair_pos then
-        return utils.delete(1)..utils.addafter(matching_pair_pos-col,utils.delete(0,1),0)
+        return utils.delete(1)..utils.addafter(matching_pair_pos-o.col,utils.delete(0,1),0)
       end
     end
   end
 end
-function M.extensions.delete_space(prev_char,_,line,col)
-  if M.conf.space and prev_char==' ' then
+function M.extensions.delete_space(o)
+  if M.conf.space and o.prev_char==' ' then
     local newcol
     local char
-    for i=col-2,1,-1 do
-      char=line:sub(i,i)
+    for i=o.col-2,1,-1 do
+      char=o.line:sub(i,i)
       if char~=' ' then
         newcol=i+2
         break
@@ -56,14 +56,14 @@ function M.extensions.delete_space(prev_char,_,line,col)
     end
     local prev_n_pair=mem.mem[char]
     if prev_n_pair and prev_n_pair.type==1 then
-      local matching_pair_pos=info_line.findepaire(line,newcol-1,char,prev_n_pair.paire)
-      if matching_pair_pos and col-newcol<matching_pair_pos-col and line:sub(matching_pair_pos-1,matching_pair_pos-1)==' ' then
-        return utils.moveh(col-newcol)..utils.delete(1)..utils.addafter(matching_pair_pos-col-1,utils.delete(0,1),-(col-newcol))
+      local matching_pair_pos=info_line.findepaire(o.line,newcol-1,char,prev_n_pair.paire)
+      if matching_pair_pos and o.col-newcol<matching_pair_pos-o.col and o.line:sub(matching_pair_pos-1,matching_pair_pos-1)==' ' then
+        return utils.moveh(o.col-newcol)..utils.delete(1)..utils.addafter(matching_pair_pos-o.col-1,utils.delete(0,1),-(o.col-newcol))
       end
     end
   end
 end
-function M.extensions.delete_multichar(_,_,line,col)
+function M.extensions.delete_multichar(o)
   if M.conf.multichar and mem.extensions.multichar then
     for newkey,opt in pairs(mem.mem) do
       local bool=#newkey>1 and opt.type~=2
@@ -71,22 +71,23 @@ function M.extensions.delete_multichar(_,_,line,col)
         bool=opt.ext.filetype[vim.o.filetype]
       end
       if bool and opt.ext.alpha and vim.tbl_contains(opt.ext.alpha.before or {},newkey) then
-        bool=not line:sub(col-#newkey-1,col-#newkey-1):match('%a')
+        bool=not o.line:sub(o.col-#newkey-1,o.col-#newkey-1):match('%a')
       end
-      if bool and line:sub(col-#newkey,col-1)==newkey and opt.paire==line:sub(col,col+#opt.paire-1) then
+      if bool and o.line:sub(o.col-#newkey,o.col-1)==newkey and opt.paire==o.line:sub(o.col,o.col+#opt.paire-1) then
         return utils.delete(#newkey,#opt.paire)
       end
     end
   end
 end
 function M.backspace(fallback)
-  local wline=utils.getline()
-  local wcol=utils.getcol()
-  local line,col=info_line.filter_string(wline,wcol,utils.getlinenr(),M.conf.notree)
-  local prev_char=line:sub(col-1,col-1)
-  local next_char=line:sub(col,col)
+  local o={}
+  o.wline=utils.getline()
+  o.wcol=utils.getcol()
+  o.line,o.col=info_line.filter_string(o.wline,o.wcol,utils.getlinenr(),M.conf.notree)
+  o.prev_char=o.line:sub(o.col-1,o.col-1)
+  o.next_char=o.line:sub(o.col,o.col)
   for _,i in pairs(M.extensions) do
-    local ret=i(prev_char,next_char,line,col) --TODO: replace argument with one table
+    local ret=i(o)
     if ret then
       return ret
     end
