b1cbb66f7542988ba1c28d6500306f97f624251b
altermo <>
2023-03-27 13:00:49 +0200

    M README.md; M lua/ultimate-autopair/defaults.lua

--- lua/ultimate-autopair/defaults.lua
+++ lua/ultimate-autopair/defaults.lua
@@ -42,7 +42,7 @@ M.default_config={
             enable=true,
             markdown={{'```','```',pair=true,noalpha=true,next=true}},
         },
-        addsemi={'c','cpp'},
+        addsemi={'c','cpp','rust'},
         fallback=nil,
         extensions=require('ultimate-autopair.maps.cr').default_extensions,
     },
