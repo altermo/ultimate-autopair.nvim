d22afdf2f555f4845da20f245978d2dc9e60abf4
altermo <>
2023-05-20 15:35:02 +0200

    fixed bug relating to cond config type

--- lua/ultimate-autopair/configs/other/cond.lua
+++ lua/ultimate-autopair/configs/other/cond.lua
@@ -1,11 +1,12 @@
+local config=require 'ultimate-autopair.config'
 local M={}
 function M.init_conf(conf,mem)
     local lmem={}
     for _,v in ipairs(conf) do
-        M.init_conf(v,lmem)
+        config.init_conf(v,lmem)
     end
     for _,v in ipairs(lmem) do
-        if v.check then
+        if v.check and conf.check then
             local check=v.check
             v.check=function (...)
                 if conf.check(...) then
@@ -13,7 +14,7 @@ function M.init_conf(conf,mem)
                 end
             end
         end
-        if v.rule then
+        if v.rule and conf.rule then
             local rule=v.rule
             v.rule=function (...)
                 if conf.rule(...) then
