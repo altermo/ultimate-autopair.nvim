f383bf7eeafbc61fa06d0a05f9b8a3b15e8c6a74
altermo <>
2023-06-29 16:06:39 +0200

    fix: filetype wont act on cmdline

--- lua/ultimate-autopair/extensions/filetype.lua
+++ lua/ultimate-autopair/extensions/filetype.lua
@@ -1,7 +1,9 @@
 local M={}
+local utils=require'ultimate-autopair.utils'
 function M.call(m,ext)
      local rule=m.rule
      m.rule=function ()
+          if utils.incmd() then return rule() end
           if ext.conf.ft and not vim.tbl_contains(ext.conf.ft,vim.o.filetype) then
           elseif ext.conf.nft and vim.tbl_contains(ext.conf.nft,vim.o.filetype) then
           elseif m.conf.ft and not vim.tbl_contains(m.conf.ft,vim.o.filetype) then
