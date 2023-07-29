289b4fa445d10df53e4eae2022d171cecc588f89
altermo <>
2023-07-29 19:01:54 +0200

    fix: multichar for alpha

--- lua/ultimate-autopair/extensions/alpha.lua
+++ lua/ultimate-autopair/extensions/alpha.lua
@@ -3,16 +3,16 @@ local default=require'ultimate-autopair.configs.default.utils'
 function M.check(o,m,ext)
     if ext.conf.alpha or m.conf.alpha then
         if o.key=='"' or o.key=="'" and vim.o.filetype=='python' and not ext.conf.no_python then
-            if vim.regex([[\v\c<((r[fb])|([fb]r)|[frub])$]]):match_str(o.line:sub(o.col-3,o.col-1)) then
+            if vim.regex([[\v\c<((r[fb])|([fb]r)|[frub])$]]):match_str(o.line:sub(o.col-1-#m.pair,o.col-#m.pair)) then
                 return
             end
         end
-        if vim.regex([[\a]]):match_str(o.line:sub(vim.str_utf_start(o.line,o.col-1)+o.col-1,o.col-1)) then
+        if vim.regex([[\a]]):match_str(o.line:sub(o.col-#m.pair,o.col-#m.pair)) then
             return true
         end
     end
-    if ext.conf.after or m.conf.alpha_after then  --TODO: test and fix
-        if vim.regex([[\a]]):match_str(o.line:sub(o.col,vim.str_utf_end(o.line,o.col)+o.col)) then
+    if ext.conf.after or m.conf.alpha_after then
+        if vim.regex([[\a]]):match_str(o.line:sub(o.col,o.col)) then
             return true
         end
     end
@@ -24,7 +24,7 @@ function M.call(m,ext)
         if M.check(o,m,ext) then return end
         return check(o)
     end
-    if ext.conf.filter then --TODO: test
+    if ext.conf.filter then
         local filter=m.filter
         m.filter=function(o)
             if M.check(o,m,ext) then return end
