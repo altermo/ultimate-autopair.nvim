ae145e13b49179d267a5e64711ad7c592815027d
altermo <>
2023-08-16 13:39:26 +0200

    feat: implemented space backspace

--- lua/ultimate-autopair/profile/default/maps/space.lua
+++ lua/ultimate-autopair/profile/default/maps/space.lua
@@ -45,6 +45,62 @@ function M.wrapp_space(m)
         return M.space(o,m)
     end
 end
+---@type prof.def.map.bs.fn
+function M.backspace(o,_,conf)
+    if not conf.space then return end
+    if o.line:sub(o.col-1,o.col-1)~=' ' then return end
+    local newcol
+    for i=o.col-2,1,-1 do
+        if o.line:sub(i,i)~=' ' then
+            newcol=i+1
+            break
+        end
+    end
+    if not newcol then return end
+    local prev_pair=default.get_pairs_by_pos(o,newcol,'start',false,function(pair)
+        return pair.conf.space
+    end)[1]
+    if not prev_pair then return end
+    local index,rindex=prev_pair.fn.find_corresponding_pair(o,newcol-#prev_pair.start_pair)
+    if not index then return end
+    if rindex~=o.row then return end
+    if o.line:sub(newcol,index-1):find('[^ ]') then
+        if conf.space=='balance' then
+            local left=#o.line:sub(newcol,index-1):match(' *')
+            local right=#o.line:sub(newcol,index-1):reverse():match(' *')
+            if left~=right then
+                return utils.create_act({
+                    {'h',left-right},
+                    {'delete',0,left-right},
+                    {'l',index-o.col-(right-left)},
+                    {'delete',0,right-left},
+                    {'h',index-o.col-(right-left)},
+                },o)
+            end
+        end
+        if o.line:sub(newcol,index-1):match(' *')
+            >o.line:sub(newcol,index-1):reverse():match(' *') then return end
+        return utils.create_act({
+            {'h'},{'delete',0,1},
+            {'l',index-o.col-1},
+            {'delete',0,1},
+            {'h',index-o.col-1},
+        },o)
+    end
+    if conf.space=='balance' then
+        local left=o.col-newcol
+        local right=index-o.col
+        if left~=right then
+            return utils.create_act({
+                {'h',left-right},
+                {'delete',0,left-right},
+                {'delete',0,right-left},
+            },o)
+        end
+    end
+    if o.line:sub(newcol,o.col-1)>o.line:sub(o.col,index-1) then return end
+    return utils.create_act({{'h'},{'delete',0,2}},o)
+end
 ---@param conf prof.def.map.space.conf
 ---@param mconf prof.def.conf
 ---@param ext prof.def.ext[]
@@ -57,13 +113,14 @@ function M.init(conf,mconf,ext)
     m.map=mconf.map~=false and conf.map
     m.cmap=mconf.cmap~=false and conf.cmap
     m.extensions=ext
-    m[default.type_def]={'space'}
+    m[default.type_def]={'space','dobackspace'}
     m.p=conf.p or mconf.p or 10
     m.doc='autopairs space key map'
 
     m.check=M.wrapp_space(m)
     m.filter=default.def_filter_wrapper(m)
     default.init_extensions(m,m.extensions)
+    m.backspace=M.backspace
     m.get_map=default.def_map_get_map_wrapper(m)
     default.extend_map_check_with_map_check(m)
     return m
