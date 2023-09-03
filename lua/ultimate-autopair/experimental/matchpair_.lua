96f49bbfdd8edcad71b13faa8a27962eab7f2ea4
altermo <>
2023-09-03 08:19:21 +0200

    feat: experimental.matchpair now has map to jump to matching paren

--- lua/ultimate-autopair/experimental/matchpair_.lua
+++ lua/ultimate-autopair/experimental/matchpair_.lua
@@ -1,15 +1,15 @@
 local default=require 'ultimate-autopair.profile.default.utils'
 local core=require'ultimate-autopair.core'
+local utils=require'ultimate-autopair.utils'
 local M={}
 ---Instruction: if you want TO USE THIS
 --use
+---require'ultimate-autopair.core'.modes={'i','c','n'}
 ---require'ultimate-autopair'.init({your_pair_config,{
 ---  profile='raw',
 ---  require'ultimate-autopair.experimental.matchpair_'.init(),
+---  require'ultimate-autopair.experimental.matchpair_'.init_map(),
 ---}})
-
---TODO:
---- maybe create matchpair map which replaces builtin map %
 ---@param m core.module
 ---@return function
 function M.wrapp_highlight_callback(m)
@@ -17,26 +17,27 @@ function M.wrapp_highlight_callback(m)
     return function ()
         vim.api.nvim_buf_clear_namespace(0,m.ns,0,-1)
         if core.disable then return end
-        ---@type prof.def.m.pair
-        local pair
         local o=core.get_o_value('')
-        for _,i in ipairs(default.filter_for_opt('pair')) do
-            ---@cast i prof.def.m.pair
-            if #i.start_pair==1 and #i.end_pair==1 and
-                i.start_pair~=i.end_pair and
-                o.line:sub(o.col,o.col)==i.pair and
-                i.filter(o) and
-                true then
-                pair=i
-                break
-            end
-        end
-        if not pair then return end
-        local col,row=pair.fn.find_corresponding_pair(o,o.col)
-        if not col then return end
+        local row,col=M.find_corresponding_pair_under_curosr(o)
+        if not row then return end
         vim.highlight.range(0,m.ns,'Visual',{o.row-1,o.col-1},{o.row-1,o.col})
         vim.highlight.range(0,m.ns,'Visual',{row-1,col-1},{row-1,col})
     end
+end
+---@param o core.o
+---@return number?
+---@return number?
+function M.find_corresponding_pair_under_curosr(o)
+    local pair,col,row=default.get_pair_and_end_pair_pos_from_start(o,o.col,nil,function (p)
+        return #p.start_pair==1 and #p.end_pair==1
+    end)
+    if not pair then
+        pair,col,row=default.get_pair_and_start_pair_pos_from_end(o,o.col,nil,function (p)
+            return #p.start_pair==1 and #p.end_pair==1
+        end)
+    end
+    if not pair then return end
+    return row,col
 end
 ---@return core.module
 function M.init()
@@ -49,6 +50,21 @@ function M.init()
         if delete then return end
         vim.api.nvim_create_autocmd('CursorMoved',{callback=M.wrapp_highlight_callback(m),desc=m.doc,group='UltimateAutopair'})
     end
+    return m
+end
+---@return core.module
+function M.init_map()
+    local m={}
+    m.doc='ultimate-autopair matchpair higlight'
+    m.p=10
+    m.check=function (o)
+        if o.mode=='n' and o.key=='%' then
+            local row,col=M.find_corresponding_pair_under_curosr(o)
+            if not row or not col then return '' end
+            return (row<o.row and ('k'):rep(o.row-row) or ('j'):rep(row-o.row))..'0'..('l'):rep(col-1)
+        end
+    end
+    m.get_map=function (mode) if mode=='n' then return {'%'} end end
     return m
 end
 return M
