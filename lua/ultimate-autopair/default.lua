35b66ad96207b4658f1cec367037f1cb9bfe48e6
altermo <107814000+altermo@users.noreply.github.com>
2024-06-11 13:22:36 +0200

    ;how should filetype specific default paris work

--- lua/ultimate-autopair/default.lua
+++ lua/ultimate-autopair/default.lua
@@ -3,7 +3,7 @@ local in_lisp=function (o)
   return (not fn.in_lisp(o)) or fn.in_string(o) or fn.in_comment(o)
 end
 --local markdown={
-  --ts_not_after={'latex_block','code_span','fenced_code_block'}
+--  ts_not_after={'latex_block','code_span','fenced_code_block'}
 --}
 return {
   conf={
@@ -13,28 +13,28 @@ return {
     {'(',')'},
     {'[',']'},
     {'{','}'},
-    {'"','"',multiline=false},
-    {"'","'",start_pair={alpha={before=true,py_fstr=true}},filter={in_lisp},multiline=false},
+    {'"','"',multiline=false,nft={'tex'}},
+    {"'","'",start_pair={alpha={before=true,py_fstr=true}},filter={in_lisp},multiline=false,nft={'tex','rust'}},
     {'`','`',filter={in_lisp},multiline=false},
-    {'```','```',ft={'markdown'}},
-    {'<!--','-->',ft={'markdown','html'}},
-    {'"""','"""',ft={'python'}},
-    {"'''","'''",ft={'python'}},
+    {'<!--','-->',ft={'markdown','html'}}, --TODO: temp
+    {'"""','"""',ft={'python'}}, --TODO: temp
+    {"'''","'''",ft={'python'}}, --TODO: temp
+    {'```','```',ft={'markdown'}}, --TODO: temp
     filter={
       cmdtype={skip={'/','?','@'}},
       escape={},
       alpha={p=-8},
       filetype={p=-9,nft={'TelescopePrompt'},lang_detect_after=true},
       tsnode={p=-10,lang_detect_after=true,separate={'comment','string','char','character',
-      'raw_string', --fish/bash/sh
-      'char_literal','string_literal', --c/cpp
-      'string_value', --css
-      'str_lit','char_lit', --clojure/commonlisp
-      'interpreted_string_literal','raw_string_literal','rune_literal', --go
-      'quoted_attribute_value', --html
-      'template_string', --javascript
-      'LINESTRING','STRINGLITERALSINGLE','CHAR_LITERAL', --zig
-      'string_literals','character_literal','line_comment','block_comment','nesting_block_comment' --d #62
+        'raw_string', --fish/bash/sh
+        'char_literal','string_literal', --c/cpp
+        'string_value', --css
+        'str_lit','char_lit', --clojure/commonlisp
+        'interpreted_string_literal','raw_string_literal','rune_literal', --go
+        'quoted_attribute_value', --html
+        'template_string', --javascript
+        'LINESTRING','STRINGLITERALSINGLE','CHAR_LITERAL', --zig
+        'string_literals','character_literal','line_comment','block_comment','nesting_block_comment' --d #62
       }},
     },
     extension={
@@ -71,66 +71,32 @@ return {
       map='<A-e>',
     }
   },
-  --TODO:
   --tex={
-  --disable={
-  --ft='tex',
-  --{"'","'"},
-  --{'`','`'},
+  --  {'``',"''",ft='tex'},
   --},
-  --set_or_change={
-  --ft='tex',
-  --{'``',"''"},
-  --{'$','$'},
-  --{'\\[','\\]'},
-  ----{'$$','$$'},
-  --}
+  --python={
+  --  {'"""','"""',ft={'python'}},
+  --  {"'''","'''",ft={'python'}},
   --},
   --markdown={
-  --set_or_change={
-  --ft='markdown',
-  --{'`','`',tsnode={ft='markdown',lang_detect='after insert',not_after={'latex_block','fenced_code_block'}}},
-  --{'*','*',tsnode={ft='markdown',lang_detect='after insert',not_after=markdown.ts_not_after}},
-  --{'_','_',tsnode={ft='markdown',lang_detect='after insert',not_after=markdown.ts_not_after}},
-  --{'__','__',tsnode={ft='markdown',lang_detect='after insert',not_after=markdown.ts_not_after}},
-  --{'**','**',tsnode={ft='markdown',lang_detect='after insert',not_after=markdown.ts_not_after}},
-  --{'$','$',tsnode={ft='markdown',lang_detect='after insert',not_after={'code_span','fenced_code_block'}}},
-  --{'~~','~~',tsnode={ft='markdown',lang_detect='after insert',not_after=markdown.ts_not_after}},
-  ----{'***','***',tsnode={ft='markdown',lang_detect='after insert',not_after=markdown.ts_not_after}},
-  ----{'___','___',tsnode={ft='markdown',lang_detect='after insert',not_after=markdown.ts_not_after}},
-  --}
-  --},
-  --rust_default_include={
-  --change={
-  --ft='rust',
-  --{"'","'",tsnode={not_after={'lifetime','label'}}},
-  --},
-  --},
-  --rust={
-  --set_or_change={
-  --ft='rust',
-  --{'<','>',
-  --tsnode={not_after={ft='rust','arrow_function','binary_expression','augmented_assignment_expression'}},
-  --multiline=function (fn) return fn.treesitter_enabled() end,
-  --},
-  --}
-  --},
-  --lua_default_include={
-  --change={
-  --ft='lua',
-  --{"'","'",alpha_before={tsnode={ft='lua',not_after={'string'}}}},
-  --},
-  --},
-  --lua={
-  --{'%[=-%[','%]=-%]',type='patter',ft='lua'},
+  --  {'```','```',ft={'markdown'},tsnode={ft='markdown',lang_detect='after insert',not_after={'latex_block','fenced_code_block'}}},
+  --  {'*','*',ft={'markdown'},tsnode={ft='markdown',lang_detect='after insert',not_after=markdown.ts_not_after}},
+  --  {'_','_',ft={'markdown'},tsnode={ft='markdown',lang_detect='after insert',not_after=markdown.ts_not_after}},
+  --  {'__','__',ft={'markdown'},tsnode={ft='markdown',lang_detect='after insert',not_after=markdown.ts_not_after}},
+  --  {'**','**',ft={'markdown'},tsnode={ft='markdown',lang_detect='after insert',not_after=markdown.ts_not_after}},
+  --  {'$','$',ft={'markdown'},tsnode={ft='markdown',lang_detect='after insert',not_after={'code_span','fenced_code_block'}}},
+  --  {'~~','~~',ft={'markdown'},tsnode={ft='markdown',lang_detect='after insert',not_after=markdown.ts_not_after}},
+  --  --{'***','***',ft={'markdown'},tsnode={ft='markdown',lang_detect='after insert',not_after=markdown.ts_not_after}},
+  --  --{'___','___',ft={'markdown'},tsnode={ft='markdown',lang_detect='after insert',not_after=markdown.ts_not_after}},
   --},
   --comment={
-  --{function (o)
-  --local utils=require'ultimate-autopair.utils'
-  --local comment=utils.ft_get_option(utils.get_filetype_after_insert(utils.to_filter(o),'´'),'commentstring') --[[@as string]]
-  --local pair={comment:match('(.+)%%s(.+)')}
-  --return #pair>0 and pair or nil
-  --end,type='callable'}
-  --},
+  --  {'%[=-%[','%]=-%]',type='patter',ft='lua'},
+  --  {function (o)
+  --    local utils=require'ultimate-autopair.utils'
+  --    local comment=utils.ft_get_option(utils.get_filetype_after_insert(utils.to_filter(o),'´'),'commentstring') --[[@as string]]
+  --    local pair={comment:match('(.+)%%s(.+)')}
+  --    return #pair>0 and pair or nil
+  --  end,type='callable',nft={'lua'}}
+  --}
 }
 --- vim:shiftwidth=2:expandtab:
