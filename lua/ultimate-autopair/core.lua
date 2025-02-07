148f27c98bddfcb3ad2e42071f7b94a6b7f99a6c
bekaboo <kankefengjing@gmail.com>
2025-02-07 13:48:48 -0500

    fix: use 'lazyredraw' to prevent cursor jump on pairing

--- lua/ultimate-autopair/core.lua
+++ lua/ultimate-autopair/core.lua
@@ -115,9 +115,23 @@ function M.get_run(key)
     end
     return 'v:lua.'..M.global_name..'.run_run("'..vim.fn.escape(key,[[\\"]])..'")'
 end
+local lz ---@type boolean? save `vim.go.lz`
 ---@param key string
 ---@return string
 function M.run_run(key)
+    -- Set 'lazyredraw' on paring to prevent cursor jump caused by `<C-g>U<Left>`
+    -- Source: https://github.com/windwp/nvim-autopairs/pull/403
+    lz = vim.go.lz
+    vim.go.lz = true
+
+    vim.schedule(function()
+      if lz == nil then
+        return
+      end
+      vim.go.lz = lz
+      lz = nil
+    end)
+
     return M.funcs[key]()
 end
 ---@param mode string
